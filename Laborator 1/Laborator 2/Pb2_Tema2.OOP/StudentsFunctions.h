28f9acfd209b4ad600619303ed5e8ab10f1bf701
Georgiana Prisecariu <prisecariugeorgiana12@gmail.com>
2026-03-02 18:58:48 +0200

    Delete Laborator 1/Laborator 2 directory

--- Laborator 1/Laborator 2/Pb2_Tema2.OOP/StudentsFunctions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Student.h"
-
-int compareName(Student s1, Student s2);
-int compareMath(Student s1, Student s2);
-int compareEnglish(Student s1, Student s2);
-int compareHistory(Student s1, Student s2);
-int compareAverage(Student s1, Student s2);
