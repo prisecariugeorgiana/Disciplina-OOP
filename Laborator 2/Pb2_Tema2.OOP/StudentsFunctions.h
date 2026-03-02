232a87e283eee694f55257e4cfcd960aa1e245db
Georgiana Prisecariu <prisecariugeorgiana12@gmail.com>
2026-03-02 19:00:16 +0200

    Add files via upload

---
+++ Laborator 2/Pb2_Tema2.OOP/StudentsFunctions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Student.h"
+
+int compareName(Student s1, Student s2);
+int compareMath(Student s1, Student s2);
+int compareEnglish(Student s1, Student s2);
+int compareHistory(Student s1, Student s2);
+int compareAverage(Student s1, Student s2);
---
+++ Laborator 2/Pb3_Tema2.OOP/MovieCompare.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Movie.h"
+
+int movie_compare_name(const Movie& m1, const Movie& m2);
+int movie_compare_year(const Movie& m1, const Movie& m2);
+int movie_compare_score(const Movie& m1, const Movie& m2);
+int movie_compare_length(const Movie& m1, const Movie& m2);
+int movie_compare_passed_years(const Movie& m1, const Movie& m2);
